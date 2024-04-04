f8d22ab64838040d8b32d24d40718b82311f4d71
aly50n <alyson.morato13@gmail.com>
2024-04-04 11:56:32 -0300

    feat: exceções e tratamento de exceções de evento.

---
+++ src/main/java/com/passin/passin/config/ExceptionEntityHandler.java
@@ -0,0 +1,15 @@
+package com.passin.passin.config;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.passin.passin.domain.event.exceptions.EventNotFoundException;
+
+@ControllerAdvice
+public class ExceptionEntityHandler {
+    @ExceptionHandler(EventNotFoundException.class)
+    public ResponseEntity handleEventNotFound(EventNotFoundException exception){
+        return ResponseEntity.notFound().build();
+    }
+}
